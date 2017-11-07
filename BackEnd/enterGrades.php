b59a4faecd75a15a33ab9b06d359dd7ed20d4f4d
Shrey123 <sr594@njit.edu>
2017-11-07 12:29:28 -0500

    adding filter questions  and updating grades

--- BackEnd/enterGrades.php
+++ BackEnd/enterGrades.php
@@ -2,7 +2,8 @@
 $studentName = $_POST['StudentName'];
 $Quiz = $_POST['QuizId'];
 $QuizScore = $_POST['Score'];
-
+$QuestionGrades = $_POST['QuestionGrades'];
+$Comment = $_POST['Comment'];
 $connection = mysqli_connect("sql2.njit.edu", "sr594", "Baseball123", "sr594");
 
 if (!$connection){
@@ -10,7 +11,7 @@ die("Connection failed: " . mysqli_connect_error());
 }
 
 
-$GradesInsert = "INSERT INTO StudentGrades (StudentUsername, Quiz, Grade) VALUES ('$studentName',$Quiz,$QuizScore);";
+$GradesInsert = "INSERT INTO StudentGrades (StudentUsername, Quiz, Grade, QuestionGrades, Comment) VALUES ('$studentName',$Quiz,$QuizScore,'$QuestionGrades','$Comment');";
 
 
 $updateTakenQuizzes = "UPDATE TakenQuizzes set Graded = 1 where username = '$studentName' and quiz_id = $Quiz;";
